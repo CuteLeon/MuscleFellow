e7610f7bb4f4c6fb2dadb88e1d3fcb1db5f44d6f
Leon <Leon.ID@QQ.COM>
2018-06-09 21:20:34 +0800

    完成Model层Order订单类和订单明细类；

---
+++ MuscleFellow.Models/Order.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MuscleFellow.Models
+{
+    /// <summary>
+    /// 订单状态
+    /// </summary>
+    public enum OrderStatus : int
+    {
+        /// <summary>
+        /// 等待支付
+        /// </summary>
+        PendingPayment,
+        /// <summary>
+        /// 等待装运
+        /// </summary>
+        PendingShipment,
+        /// <summary>
+        /// 等待计价
+        /// </summary>
+        PendingEvaluated,
+        /// <summary>
+        /// 已关闭
+        /// </summary>
+        Closed,
+        /// <summary>
+        /// 已取消
+        /// </summary>
+        Cancelled
+    }
+    public class Order
+    {
+        [Key]
+        [DisplayName("订单ID")]
+        public Guid OrderID { get; set; }
+
+        [DisplayName("用户ID")]
+        public string UserID { get; set; }
+
+        [DisplayName("订单项目")]
+        public List<OrderDetail> OrderItems { get; set; }
+
+        [DisplayName("地址")]
+        [MaxLength(128)]
+        public string Address { get; set; }
+
+        [DisplayName("总价")]
+        public float TotalPrice { get; set; }
+
+        [DisplayName("订单状态")]
+        public OrderStatus OrderStatus { get; set; }
+
+        [DisplayName("下单时间")]
+        public DateTime? OrderDate { get; set; }
+    }
+}
---
+++ MuscleFellow.Models/OrderDetails.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MuscleFellow.Models
+{
+    public class OrderDetail
+    {
+        [DisplayName("订单明细ID")]
+        public int OrderDetailID { get; set; }
+
+        [DisplayName("订单ID")]
+        public Guid OrderID { get; set; }
+
+        [DisplayName("商品ID")]
+        public Guid ProductID { get; set; }
+
+        [DisplayName("产品名称")]
+        public string ProductName { get; set; }
+
+        [DisplayName("缩略图路径")]
+        public string ThumbImagePath { get; set; }
+
+        [DisplayName("数量")]
+        public int Quantity { get; set; }
+
+        [DisplayName("单价")]
+        public float UnitPrice { get; set; }
+
+        [DisplayName("小计")]
+        public float SubTotal { get; set; }
+
+        [DisplayName("下单时间")]
+        public DateTime? PlaceDate { get; set; }
+    }
+}
